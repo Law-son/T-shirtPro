09a30e2bd759ef498621312590da488bc7d74b04
Lawson Buabassah <buabassahlawson@gmail.com>
2022-11-04 01:41:13 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //application routes
       routes: {
         //  '/': (context) => const MyHomePage(),
         '/home': (context) => const MyHomePage(),
